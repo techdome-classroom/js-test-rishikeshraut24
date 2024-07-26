25a3b133552557b53b5e46d12a29eceb27c83f14
ashutosh <ashupra222@gmail.com>
2024-07-26 13:27:48 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,38 +1,34 @@
 const getTotalIsles = function (grid) {
+  if (grid.length === 0) return 0;
 
-
-  // write your code here
-  if(grid.length === 0) return 0;
-  let count =0;
+  let count = 0;
   const rows = grid.length;
+  const cols = grid[0].length;
 
-  const cols =grid[0].length;
-
-  function dfs(r ,c){
-    if(r < 0 || c<0 || r >= rows || c >= cols || grid[r][c] == 'w'){
+  function dfs(r, c) {
+    if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] === 'w') {
       return;
     }
 
-    grid[r][c] = 'W';
+    grid[r][c] = 'w'; // Mark the current cell as visited
 
-    dfs(r -1, c);
-    dfs(r +1, c);
-    dfs(r ,c - 1);
-    dfs(r ,c+ c);
-
+    // Check all four directions
+    dfs(r - 1, c); // up
+    dfs(r + 1, c); // down
+    dfs(r, c - 1); // left
+    dfs(r, c + 1); // right
   }
-  for(let i=0; i<rows; i++){
-    for(let j=0; j<cols;j++){
 
-      if(grid[i][j] === 'L'){
-        dfs(i,j);
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === 'L') {
+        dfs(i, j);
         count++;
       }
     }
   }
+
   return count;
-
-
 };
 
 module.exports = getTotalIsles;
