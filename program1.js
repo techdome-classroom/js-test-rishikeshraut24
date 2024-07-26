a9923f4e076f0cbf932f3b8680afccec0a214d22
ashutosh <ashupra222@gmail.com>
2024-07-26 12:49:24 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -9,7 +9,7 @@ const getTotalIsles = function (grid) {
   const cols =grid[0].length;
 
   function dfs(r ,c){
-    if(r < 0 || c<)
+    if(r < 0 || c<0 || r >= rows || c >= cols || grid[r][c] == '0')
   }
 
 
