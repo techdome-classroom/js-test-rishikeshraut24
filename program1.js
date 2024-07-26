0fdc84774df767ec72fc59afa6834a514cc4713c
ashutosh <ashupra222@gmail.com>
2024-07-26 12:44:48 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -4,8 +4,10 @@ const getTotalIsles = function (grid) {
   // write your code here
   if(grid.length === 0) return 0;
   let count =0;
-  const rows = grid,length;
-  
+  const rows = grid.length;
+
+  const cols =
+
 
 };
 
